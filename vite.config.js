6fd8ac3b6431edc3e4fbce5cbd5f6949070b13e2
Abhikori <abhishekkori7902@gmail.com>
2024-10-17 18:12:12 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Abhishek-Portfolio/"
+  base: "/Abhishek-Portfolio/",
+  json: {
+    namedExports: true,
+  }
 })
